32f5e77f4592c7e1e643e31aa4c69c815552695e
warrendong <18135834940@163.com>
2018-10-22 11:50:03 +0800

    day7

---
+++ day7/WebContent/loginsucc.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<title>登录成功页面</title>
+</head>
+<body>
+	登录成功
+</body>
+</html>
---
+++ day7/src/com/cn/server/Loginservlet.java
@@ -0,0 +1,56 @@
+package com.cn.server;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Loginservlet
+ */
+@WebServlet("/Loginservlet")
+public class Loginservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Loginservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name=request.getParameter("name");
+		String pwd=request.getParameter("pwd");
+		String user="zhangsan";
+		String pass="123456";
+		if(name==user&&pwd==pass) {
+			response.sendRedirect("loginsucc.jsp");
+		}else {
+			response.sendRedirect("");
+		}
+	}
+
+}
